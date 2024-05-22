188d848efcbe2988389e2fc2e924a514775f5248
Adrian M. do Nascimento Alves <adrianmoizes123@gmail.com>
2024-05-21 23:40:38 -0300

    criação do menu responsivo

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 const pontos = document.getElementById('pontos');
 const leiaMais = document.getElementById('mais');
 const btnLerMais = document.getElementById('btnLerMais')
-
+//função para ler mais
 function lerMais () {
     if(pontos.style.display === "none"){
         pontos.style.display="inline";
@@ -13,3 +13,19 @@ function lerMais () {
         btnLerMais.innerHTML = "Leia Menos";
     }
 }
+//função para o menu responsivo
+document.getElementById('open-btn').addEventListener('click',function(){
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+    document.getElementById('content').classList.toggle('open-bar');
+});
+
+//função para os background dos links no menu
+
+const sideItems = document.querySelectorAll('.side-item');
+
+sideItems.forEach(item => {
+    item.addEventListener('click', () => {
+        sideItems.forEach(i => i.classList.remove('active'));
+        item.classList.add('active');
+    });
+});
